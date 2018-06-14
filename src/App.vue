4b92247be2cfcc45c0b461fcd5db6523aa676268
unknown <tarunkumarv@8389VM.charnwood.local>
2018-06-14 09:48:57 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+// Top level component that serves as the
+// framework for the entire application
+
+// Template Section
+// Visual part of the component
+<template>
+  <div>
+    <!-- render the TodoList compoent - note that camelcase (TodoLis) is changed to snakecase (todo-list) -->
+    <todo-list></todo-list>
+  </div>
+</template>
+
+// Component Class Section
+// Behaviour, events, data storage part of the component
+<script>
+// import our component
+import TodoList from './components/TodoList'
+
+export default {
+  // reference to the TodoList component in the compoents property
+  components: {
+    TodoList
+  }
+}
+</script>
+
+// Style Section
+// Improve the appearance of the template within the component
+<style>
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+// TodoList component
+
+// Template Section
+// Visual part of the component
+<template>
+  <div>
+    <ul>
+        <li> Todo A </li>
+        <li> Todo B </li>
+        <li> Todo C </li>
+    </ul>
+  </div>
+</template>
+
+// Component Class Section
+// Behaviour, events, data storage part of the component
+<script type = "text/javascript" >
+
+export default {
+}
+</script>
+// Style Section
+// Improve the appearance of the template within the component
+<style>
+</style>
