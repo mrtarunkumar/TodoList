43165b5a2ce847e680d67ca0a816625de6d548bc
mrtarunkumar <tarunkumar.vegamwala@ntlworld.com>
2018-06-14 11:47:24 +0100

    Updated App.vue, TodoList.vue and Render Data

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+app
 // Top level component that serves as the
 // framework for the entire application
 
@@ -5,8 +6,12 @@
 // Visual part of the component
 <template>
   <div>
-    <!-- render the TodoList compoent - note that camelcase (TodoLis) is changed to snakecase (todo-list) -->
-    <todo-list></todo-list>
+    <!--
+    // render the TodoList compoent - note that camelcase (TodoLis) is changed to snakecase (todo-list)
+    // pass data from the main compoent to the TodoList compoent by
+    // using v-bind directive which takes an argument
+    -->
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
@@ -17,9 +22,33 @@
 import TodoList from './components/TodoList'
 
 export default {
+  name: 'app',
   // reference to the TodoList component in the compoents property
   components: {
     TodoList
+  },
+  // data function avails data to the template
+  // TodoList data have three properties, title, project, done
+  data () {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false
+      }]
+    }
   }
 }
 </script>
@@ -27,4 +56,12 @@ export default {
 // Style Section
 // Improve the appearance of the template within the component
 <style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,22 +3,43 @@
 // Template Section
 // Visual part of the component
 <template>
-  <div>
-    <ul>
-        <li> Todo A </li>
-        <li> Todo B </li>
-        <li> Todo C </li>
-    </ul>
+<div>
+  <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+  <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+  // Loop through using v-for="item in items" syntax
+  <div class='ui centered card' v-bind:key="todo.title" v-for="todo in todos">
+    <div class='content'>
+      <div class='header'>
+          {{ todo.title }}
+        </div>
+        <div class='meta'>
+          {{ todo.project }}
+        </div>
+        <div class='extra content'>
+          <span class='right floated edit icon'>
+            <i class='edit icon'></i>
+          </span>
+        </div>
+    </div>
+
+      <div class='ui bottom attached green basic button' v-show="todo.done">
+        Completed
+      </div>
+      <div class='ui bottom attached red basic button' v-show="!todo.done">
+        Complete
+      </div>
   </div>
+</div>
 </template>
 
 // Component Class Section
 // Behaviour, events, data storage part of the component
 <script type = "text/javascript" >
-
 export default {
+  props: ['todos']
 }
 </script>
+
 // Style Section
 // Improve the appearance of the template within the component
 <style>
